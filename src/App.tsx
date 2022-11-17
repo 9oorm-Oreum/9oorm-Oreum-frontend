e1b9e5dcae8c64bfbb7042decc50eee48f64e89a
wish0ne <wish@khu.ac.kr>
2022-11-17 11:23:00 +0900

    test: 카카오 메세지 공유 api 연결 테스트(#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,96 @@
+import { useEffect } from "react";
+
+
 function App() {
-  return <div></div>;
+
+
+  useEffect(()=>{
+    
+  },[])
+
+  const shareKakao = () =>{
+    if((window as any).Kakao){
+      const kakao = (window as any).Kakao;
+      if(!kakao.isInitialized()){
+        kakao.init('6583918a36b4a2c18eb58804d4bb3e50');
+      }
+      kakao.Share.createDefaultButton({
+        container: '#kakaotalk-sharing-btn',
+        objectType: 'feed',
+        content: {
+          title: '오늘의 디저트',
+          description: '아메리카노, 빵, 케익',
+          imageUrl:
+            'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+          link: {
+            mobileWebUrl: 'https://developers.kakao.com',
+            webUrl: 'https://developers.kakao.com',
+          },
+        },
+        itemContent: {
+          profileText: 'Kakao',
+          profileImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+          titleImageUrl: 'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+          titleImageText: 'Cheese cake',
+          titleImageCategory: 'Cake',
+          items: [
+            {
+              item: 'Cake1',
+              itemOp: '1000원',
+            },
+            {
+              item: 'Cake2',
+              itemOp: '2000원',
+            },
+            {
+              item: 'Cake3',
+              itemOp: '3000원',
+            },
+            {
+              item: 'Cake4',
+              itemOp: '4000원',
+            },
+            {
+              item: 'Cake5',
+              itemOp: '5000원',
+            },
+          ],
+          sum: 'Total',
+          sumOp: '15000원',
+        },
+        social: {
+          likeCount: 10,
+          commentCount: 20,
+          sharedCount: 30,
+        },
+        buttons: [
+          {
+            title: '웹으로 이동',
+            link: {
+              mobileWebUrl: 'https://developers.kakao.com',
+              webUrl: 'https://developers.kakao.com',
+            },
+          },
+          {
+            title: '앱으로 이동',
+            link: {
+              mobileWebUrl: 'https://developers.kakao.com',
+              webUrl: 'https://developers.kakao.com',
+            },
+          },
+        ],
+      });
+    }
+  
+  }
+  
+  return  <button onClick={shareKakao}>
+  <img
+    src="https://developers.kakao.com/assets/img/about/logos/kakaolink/kakaolink_btn_medium.png"
+    alt="카카오링크 보내기 버튼"
+    id="kakaotalk-sharing-btn"
+  />
+</button>
 }
 
 export default App;
