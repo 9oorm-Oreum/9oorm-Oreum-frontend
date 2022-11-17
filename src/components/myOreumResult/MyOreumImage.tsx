f97e02839fd614eb2a888ecb9ac7b0e43c2873c8
jynam <njy1007@gmail.com>
2022-11-17 21:54:49 +0900

    feat: 내 오름 생성 결과 첫 페이지 퍼블리싱

--- src/components/myOreumResult/MyOreumImage.tsx
+++ src/components/myOreumResult/MyOreumImage.tsx
@@ -1,3 +1,11 @@
-export default function MyOreumImage() {
-  return <div></div>;
+import styled from 'styled-components';
+
+export default function MyOreumImage({ className }: { className?: string }) {
+  return <Container className={className}>MyOreumImage</Container>;
 }
+
+const Container = styled.div`
+  height: 243px;
+  background-color: gray;
+  width: 100%;
+`;
--- src/pages/MyOreumResult.tsx
+++ src/pages/MyOreumResult.tsx
@@ -1,20 +1,18 @@
 import Title from '../components/myOreumResult/Title';
 import styled from 'styled-components';
 import MyOreumName from '../components/myOreumResult/MyOreumName';
-import { useEffect } from 'react';
+import MyOreumImage from '../components/myOreumResult/MyOreumImage';
+import CtaButton from '../components/myOreumResult/CtaButton';
 
 export default function MyOreumResultPage() {
-  useEffect(() => {
-    fetch('https://9oorm-oreum-backend.shop/oreums/1')
-      .then((r) => r.json())
-      .then((d) => console.log(d));
-  }, []);
-
   return (
     <Container>
-      <Title content='손은서' />
+      <StyledTitle content='손은서' />
       <StyledMyOreumName content='거문오름' />
-      {/* <OreumType /> */}
+      <OreumType>말굽형 오름</OreumType>
+      <StyledMyOreumImage />
+      <Description> {`제주도에 실제로 있는 나만의 오름이에요\n오름에 대해 더 알아볼까요?`}</Description>
+      <StyledCtaButton>나만의 오름 알아보기</StyledCtaButton>
     </Container>
   );
 }
@@ -23,8 +21,44 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  height: 100vh;
+  background-color: #49a098;
+  padding: 0 15px;
 `;
 
 const StyledMyOreumName = styled(MyOreumName)`
   margin-top: 27px;
+`;
+
+const OreumType = styled.div`
+  font-family: 'Pretendard';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 17px;
+  line-height: 22px;
+  color: #ffffff;
+  margin-top: 12px;
+`;
+
+const StyledMyOreumImage = styled(MyOreumImage)`
+  margin-top: 9px;
+`;
+
+const StyledTitle = styled(Title)`
+  margin-top: 59px;
+`;
+
+const Description = styled.div`
+  font-weight: 500;
+  font-style: normal;
+  font-size: 18px;
+  line-height: 160%;
+  color: #ffffff;
+  white-space: pre-line;
+  text-align: center;
+  margin-top: 24px;
+`;
+
+const StyledCtaButton = styled(CtaButton)`
+  margin-top: 103px;
 `;
