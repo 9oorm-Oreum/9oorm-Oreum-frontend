9fcb2eba6d57bee31060f4f5488d63b3c85406d6
jynam <njy1007@gmail.com>
2022-11-17 18:21:19 +0900

    refactor: 컴포넌트 명 수정

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import HomePage from '../pages/Home';
-import ResultPage from '../pages/Result';
+import ResultPage from '../pages/MyOreumResult';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/*" element={<HomePage />} />
-        <Route path="/result" element={<ResultPage />} />
+        <Route path='/*' element={<HomePage />} />
+        <Route path='/result' element={<ResultPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/MyOreumResult.tsx
@@ -0,0 +1,3 @@
+export default function MyOreumResultPage() {
+  return <div></div>;
+}
