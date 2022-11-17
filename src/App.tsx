f39d39896dabccfc090a4e8e07958474f01ca9dc
jynam <njy1007@gmail.com>
2022-11-17 11:39:32 +0900

    feat: 카카오 지도에 커스텀 이미지 마커 찍기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,106 @@
+import { useEffect } from 'react';
+
 function App() {
-  return <div></div>;
+  const kakao = (window as any).kakao;
+  console.log(kakao);
+  useEffect(() => {
+    const container = document.getElementById('myMap');
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+    const map = new kakao.maps.Map(container, options);
+    const markerImage = new kakao.maps.MarkerImage(
+      'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
+      new kakao.maps.Size(64, 69),
+      { offset: new kakao.maps.Point(27, 69) }
+    );
+    var marker = new kakao.maps.Marker({
+      position: new kakao.maps.LatLng(33.450701, 126.570667),
+      image: markerImage, // 마커이미지 설정
+    });
+    marker.setMap(map);
+
+    const Kakao = (window as any).Kakao;
+    Kakao.Share.createDefaultButton({
+      container: '#kakaotalk-sharing-btn',
+      objectType: 'feed',
+      content: {
+        title: '오늘의 디저트',
+        description: '아메리카노, 빵, 케익',
+        imageUrl:
+          'https://mud-kage.kakao.com/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+          webUrl: 'https://developers.kakao.com',
+        },
+      },
+      itemContent: {
+        profileText: 'Kakao',
+        profileImageUrl:
+          'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+        titleImageUrl:
+          'https://mud-kage.kakao.com/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+        titleImageText: 'Cheese cake',
+        titleImageCategory: 'Cake',
+        items: [
+          {
+            item: 'Cake1',
+            itemOp: '1000원',
+          },
+          {
+            item: 'Cake2',
+            itemOp: '2000원',
+          },
+          {
+            item: 'Cake3',
+            itemOp: '3000원',
+          },
+          {
+            item: 'Cake4',
+            itemOp: '4000원',
+          },
+          {
+            item: 'Cake5',
+            itemOp: '5000원',
+          },
+        ],
+        sum: 'Total',
+        sumOp: '15000원',
+      },
+      social: {
+        likeCount: 10,
+        commentCount: 20,
+        sharedCount: 30,
+      },
+      buttons: [
+        {
+          title: '웹으로 이동',
+          link: {
+            mobileWebUrl: 'https://developers.kakao.com',
+            webUrl: 'https://developers.kakao.com',
+          },
+        },
+        {
+          title: '앱으로 이동',
+          link: {
+            mobileWebUrl: 'https://developers.kakao.com',
+            webUrl: 'https://developers.kakao.com',
+          },
+        },
+      ],
+    });
+  }, []);
+
+  return (
+    <div
+      id="myMap"
+      style={{
+        width: '100%',
+        height: '500px',
+      }}
+    ></div>
+  );
 }
 
 export default App;
